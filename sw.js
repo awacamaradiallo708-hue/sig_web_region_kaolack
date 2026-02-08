82318c1802e290659885d18b070e4a6054bf8650
awacamaradiallo708-hue <awacamaradiallo708@gmail.com>
2026-02-08 21:59:37 +0000

    Mise à jour desing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // c:\xampp\tomcat\webapps\cartographie_web_region_kaolack\sw.js
-const CACHE_NAME = 'sig-kaolack-v4';
+const CACHE_NAME = 'sig-kaolack-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
